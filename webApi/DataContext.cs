fdab2e68ddd6691bb7e472a34025aa06129a19b6
seminar <seminar@DOMAIN.ORG>
2025-03-18 11:44:44 +0200

    web api first deploy

---
+++ webApi/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace webApi
+{
+    public class DataContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAB-D-16;Database=userDB;" +
+               "User Id=DOMAIN\\seminar;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ webApi/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+
+    {
+       
+        private readonly DataContext _context;
+
+
+        public UserController()
+        {
+            _context = new DataContext();
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _context.Users.ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _context.Users.First(u => u.Id == id);
+        }
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User updateUser)
+        {
+            User user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                throw new Exception("Not Exist for Update");
+            }
+            user.Id = updateUser.Id;
+            user.Name = updateUser.Name;
+            user.Email = updateUser.Email;
+
+            _context.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            User user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                throw new Exception("Not Exist for Delete");
+            }
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+    }
+
+}
